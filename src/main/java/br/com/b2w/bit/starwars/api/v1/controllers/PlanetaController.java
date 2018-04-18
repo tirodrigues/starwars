24d1ec4d6298eee553572f141ba0ab92552ef3c9
tirodrigues <tirodrigues.jpa@gmail.com>
2018-04-18 15:11:50 -0300

    troca do feign pelo resttemplate

--- src/main/java/br/com/b2w/bit/starwars/api/v1/controllers/PlanetaController.java
+++ src/main/java/br/com/b2w/bit/starwars/api/v1/controllers/PlanetaController.java
@@ -1,12 +1,18 @@
 package br.com.b2w.bit.starwars.api.v1.controllers;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.b2w.bit.starwars.api.v1.commands.PlanetaDTO;
 import br.com.b2w.bit.starwars.api.v1.converters.PlanetaDTOToPlaneta;
@@ -55,7 +62,18 @@ public class PlanetaController {
 	public void save(@Valid @RequestBody PlanetaDTO planetaDTO) {
 		SWClient client = Feign.builder().target(SWClient.class, "https://swapi.co/api/planets/?search={nome}");
 		
-		PlanetaClient buscaPlanetaByName = client.buscaPlanetaByName(planetaDTO.getNome());
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("user-agent", "B2W.bit");
+		headers.setAccept(Arrays.asList(new MediaType[] { MediaType.APPLICATION_JSON }));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		
+		RestTemplate restTemplate = new RestTemplate();
+		HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+		
+		//ResponseEntity<PlanetaClient> respEntity = restTemplate.exchange("https://swapi.co/api/planets/?search=Yavin", HttpMethod.GET, entity, PlanetaClient.class);
+		Object response = restTemplate.exchange("https://swapi.co/api/planets/?search=Yavin", HttpMethod.GET,entity,Object.class);
+		
+		//PlanetaClient body = respEntity.getBody();
 		
 		planetaService.save(planetaDTOToPlaneta.convert(planetaDTO));
 	}
