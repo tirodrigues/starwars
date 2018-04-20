cf27979c43a0ca01b15152fe7fafc9e15f00e1be
tirodrigues <tirodrigues.jpa@gmail.com>
2018-04-20 00:55:30 -0300

    add README.md

---
+++ README.md
@@ -0,0 +1,18 @@
+# StarWars
+API REST para cadastro de planetas desafio B2W.
+
+## Tecnologias
+
+-JAVA 8
+-SPRING BOOT
+-MONGODB
+
+## RUN MAVEN
+Com o banco devidamento configurado em "/starwars/src/main/resources.application.properties"
+
+Executar: ``` mvn spring-boot:run ``` 
+
+## Documentação
+http://localhost:8080/swagger-ui.html
+
+
--- src/main/java/br/com/b2w/bit/starwars/api/v1/bootstrap/Bootstrap.java
+++ src/main/java/br/com/b2w/bit/starwars/api/v1/bootstrap/Bootstrap.java
@@ -1,38 +1,12 @@
 package br.com.b2w.bit.starwars.api.v1.bootstrap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-
-import br.com.b2w.bit.starwars.api.v1.models.Planeta;
-import br.com.b2w.bit.starwars.api.v1.repositories.PlanetaRepository;
 
 @Component
 public class Bootstrap implements CommandLineRunner {
 
-	@Autowired
-	private PlanetaRepository planetaRepository;
-
 	@Override
 	public void run(String... args) throws Exception {
-
-		System.out.println("Deletando todo mundo deleteAll():");
-		planetaRepository.deleteAll();
-
-		// save planeta
-		Planeta planeta = new Planeta();
-		planeta.setNome("XYZ");
-		planeta.setClima("Chuvoso");
-		planeta.setTerreno("Gelido");
-		planeta.setQuantidadeFilmes(2L);
-
-		Planeta planeta2 = new Planeta();
-		planeta2.setNome("Krypton");
-		planeta2.setClima("Chuvoso");
-		planeta2.setTerreno("Arenoso");
-		planeta2.setQuantidadeFilmes(1L);
-
-		planetaRepository.save(planeta);
-		planetaRepository.save(planeta2);
 	}
 }
